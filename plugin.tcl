0bc9792c324206f1afa300e6c6500b99a11d5035
Damian <damian@brakel.com.au>
2024-08-06 09:51:35 +1000

    Update plugin.tcl
    Roll back 1.3 to v1.2 state

--- plugin.tcl
+++ plugin.tcl
@@ -6,20 +6,12 @@ if {![info exist ::settings(auto_load_profile_filename)]} {
     set ::settings(auto_load_profile_title) No
 }
 
-rename app_exit ::app_exit_original
-proc ::app_exit {} {
-    ::plugins::auto_load_profile::load_profile
-    app_exit_original
-}
-
 namespace eval ::plugins::${plugin_name} {
     variable author "Damian"
     variable contact "via Diaspora"
     variable description "This app extension allows you to set a profile to auto load when the app starts or wakes from sleep"
-    variable version 1.3
+    variable version 1.4
     variable min_de1app_version {1.40.1}
-
-    variable active 0
 
     proc build_ui {} {
         # Unique name per page
@@ -38,15 +30,20 @@ namespace eval ::plugins::${plugin_name} {
         set font "notosansuiregular"
         set font_bold "notosansuibold"
 
-        variable active 1
+
 
         dui add canvas_item rect $page_name 0 0 2560 1600 -fill $background_colour -width 0
         dui add dtext $page_name 1280 240 -text [translate "Auto Load Profile"] -font [dui font get $font_bold 28] -fill $text_colour -anchor "center" -justify "center"
         dui add variable $page_name 1280 320 -font [dui font get $font 16] -fill $orange -anchor center -width 1800 -textvariable {[::plugins::auto_load_profile::skin_check]}
+
         dui add variable $page_name 2510 1560 -font [dui font get $font 12] -fill $text_colour -anchor e -justify right -textvariable {Version $::plugins::auto_load_profile::version  by $::plugins::auto_load_profile::author}
+
         dui add variable $page_name 990 600 -font [dui font get $font 18] -fill $text_colour -anchor e -justify right -width 800 -textvariable {auto load profile = $::settings(auto_load_profile_title)}
         dui add variable $page_name 990 740 -font [dui font get $font 18] -fill $text_colour -anchor e -justify right -textvariable {current profile = $::settings(profile_title)}
         dui add variable $page_name 1660 650 -font [dui font get $font 16] -fill $orange -anchor w -width 600 -textvariable {$::settings(auto_load_profile_title) [translate "profile will automatically load when the app starts up or wakes from sleep"]}
+
+        if {$::settings(skin) == "DSx2" || $::settings(skin) == "DSx"} {
+            }
         dui add dbutton $page_name 1030 540 \
             -bwidth 500 -bheight 120 \
             -shape round -fill $foreground_colour -radius 60 \
@@ -62,9 +59,9 @@ namespace eval ::plugins::${plugin_name} {
             -shape round -fill $foreground_colour -radius 60 \
             -label [translate "Exit"] -label_font [dui font get $font_bold 18] -label_fill $button_label_colour -label_pos {0.5 0.5} \
             -command {if {$::settings(skin) == "DSx"} {restore_DSx_live_graph}; set_next_page off off; dui page load off; }
+
     return $page_name
     }
-
     proc skin_check {} {
         if {$::settings(skin) == "DSx2" || $::settings(skin) == "DSx"} {
             return [translate "$::settings(skin) has it's own auto load feature, settings for this extension will be ignored"]
@@ -72,9 +69,8 @@ namespace eval ::plugins::${plugin_name} {
             return ""
         }
     }
-
     proc load_profile { args } {
-        if {$::settings(skin) == "DSx2" || $::settings(skin) == "DSx" || $::plugins::auto_load_profile::active == 0} {
+        if {$::settings(skin) == "DSx2" || $::settings(skin) == "DSx"} {
             return
         }
         if {$::settings(auto_load_profile_filename) != "No"} {
@@ -96,7 +92,7 @@ namespace eval ::plugins::${plugin_name} {
     }
 
     proc auto_load_profile_listener {} {
-        # ::register_state_change_handler Sleep Idle ::plugins::auto_load_profile::load_profile
+        ::register_state_change_handler Sleep Idle ::plugins::auto_load_profile::load_profile
     }
 
     proc main {} {
